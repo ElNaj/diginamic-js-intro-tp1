eb9cd0c0723d85135afbb64e13b1c6900a91fb82
ElNajim <nelfateoui@gmail.com>
2019-10-15 16:28:23 +0200

    Excercice1 fait, celui à garder pour la vérif du Tp

--- exercice-1.js
+++ exercice-1.js
@@ -9,10 +9,10 @@ function suiteFibona(){
     let i = 0; //Premier élément de la suite        
     let j = 1; //Deuxième élément de la suite
     
-    console.log("Elément = " + i);
-    console.log("Elément = " + j);
+    console.log("1er Elément = " + i);
+    console.log("2eme Elément = " + j);
     
-    for(let k=0 ; k<=20 ; k++){
+    for(let k=0 ; k<18 ; k++){ // k < 18 car les deux premiers éléments sont i et j
         let c = i+j;
         i = j;
         j = c;
